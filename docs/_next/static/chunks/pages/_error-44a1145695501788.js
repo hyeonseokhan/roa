a52a6dc9caa408e63df7c5fea6fd092eb366297c
hyeonseokhan <toycode727@gmail.com>
2024-11-29 11:20:31 +0900

    :white_check_mark: 빌드시 퍼블리싱 정적 데이터 디렉토리 docs 추가 완료

---
+++ docs/_next/static/chunks/pages/_error-44a1145695501788.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{3177:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(2988)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[888,774,179],()=>n(3177)),_N_E=_.O()}]);
