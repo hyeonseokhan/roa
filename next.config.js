f473c668e1e7eaf8cf7991c730c7a5c6ac3b72dc
hyeonseokhan <toycode727@gmail.com>
2024-11-29 14:10:33 +0900

    :white_check_mark: 재배포

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withImages = require('next-images');
 const { homepage } = require('./package.json');
 const { NODE_ENV } = process.env;
 
-module.exports = {
+module.exports = withImages({
   basePath:
     NODE_ENV === 'prod' && homepage
       ? new URL(homepage).pathname.replace(/\/$/, '') || ''
       : '',
   output: 'export',
   distDir: 'docs',
-};
+});
