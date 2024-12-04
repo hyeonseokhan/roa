f8db848ab41d28f5bff9eac49c28b58ea07d6c64
hyeonseokhan <toycode727@gmail.com>
2024-12-04 16:52:32 +0900

    :white_check_mark: 수령골드 수수료 안내문구 포함

--- app/gold-calculator/history-table.tsx
+++ app/gold-calculator/history-table.tsx
@@ -12,10 +12,24 @@ export function HistoryTable() {
   };
   return (
     <div className="space-y-6">
-      <h1 className="text-xl font-bold text-gray-300">골드 계산 이력</h1>
-
+      <div className="grid grid-cols-2">
+        <h1 className="text-xl font-bold text-gray-300">골드 계산 이력</h1>
+        <div className="flex items-end justify-end">
+          <img
+            className="m-0 mr-1 h-4 w-4"
+            alt="gold"
+            src="https://cdn.rloa.gg/icons/gold.png"
+          />
+          <span className="text-xs font-semibold text-gray-100">
+            수령 골드
+          </span>
+          <span className="text-xs font-normal text-gray-300">
+            : 거래 수수료 -5% 적용
+          </span>
+        </div>
+      </div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right rounded-[0.5rem] overflow-hidden">
+        <table className="w-full overflow-hidden rounded-[0.5rem] text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
           <thead className="bg-gray-200 text-center text-xs uppercase text-gray-1000 dark:text-gray-400">
             <tr>
               <th scope="col" className="py-3">
